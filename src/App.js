5064956c14208162a114fc0eba805859f95789eb
Vitalik Kim <kimv1086@gmail.com>
2019-11-23 11:59:09 +0600

    create menu component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import foodImg from './assets/food-img.png';
+import drinkImg from './assets/drink-img.png';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+
+    items: [
+      {id: 1, count: 0, name: 'Hamburger', price: 80, image: foodImg},
+      {id: 1, count: 0, name: 'Cheeseburger', price: 90, image: foodImg},
+      {id: 1, count: 0, name: 'Fries', price: 45, image: foodImg},
+      {id: 1, count: 0, name: 'Coffee', price: 70, image: drinkImg},
+      {id: 1, count: 0, name: 'Tea', price: 50, image: drinkImg},
+      {id: 1, count: 0, name: 'Cola', price: 40, image: drinkImg},
+    ],
+    totalPrice: 0
+
+  };
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/MenuItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MenuItem = props => (
+    <div className="menu-item" onClick={props.onAddItem}>
+        <div className="menu-item__img">
+            <img src={props.image} alt={props.name}/>
+        </div>
+        <div className="menu-item__txt">
+            <h4 className="menu-item__title">{props.name}</h4>
+            <span>Price: {props.price} KGS</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
